413021f7d5c967cb036ced4b50500a2c589892ef
Masahiro Kanai <mkanai@broadinstitute.org>
2022-09-23 19:41:47 -0400

    modify breaks for loglog_p

--- R/trans_loglog_p.R
+++ R/trans_loglog_p.R
@@ -1,17 +1,24 @@
 #' Title
 #'
-#' @param loglog_p 
+#' @param loglog_p
 #'
 #' @return
 #' @export
 #'
 #' @examples
 trans_loglog_p <- function(loglog_p = 10) {
+  .transform = function(x) {
+    ifelse(x < loglog_p, x, loglog_p * log10(x) / log10(loglog_p))
+  }
+  .inverse = function(x) {
+    ifelse(x < loglog_p, x, 10 ** (log10(loglog_p) * x / loglog_p))
+  }
   scales::trans_new(
     "loglog_p",
-    transform = function(x)
-      ifelse(x < loglog_p, x, loglog_p * log10(x) / log10(loglog_p)),
-    inverse = function(x)
-      ifelse(x < loglog_p, x, 10 ** (log10(loglog_p) * x / loglog_p))
+    transform = .transform,
+    inverse = .inverse,
+    breaks = function(x, n = 5) {
+      round(.inverse(scales::extended_breaks(n = n)(x = .transform(x))))
+    }
   )
 }
