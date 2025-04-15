26e691b54fba28cf782dc86c84cf25c854fa190c
Masahiro Kanai <mkanai@broadinstitute.org>
2025-04-15 10:00:05 -0400

    fix scale_color_chromosome

--- R/scale_color_chromosome.R
+++ R/scale_color_chromosome.R
@@ -7,9 +7,9 @@
 #' @export
 #'
 #' @examples
-scale_color_chromosome = function(odd_color = "darkblue", even_color = "grey50") {
-  chromosomes = locusviz:::CHROMOSOME_SIZES[["GRCh37"]]$chromosome
-  chromosome_colors = rep(c("darkblue", "grey50"), length.out = length(chromosomes)) %>%
+scale_color_chromosome = function(odd_color = "darkblue", even_color = "grey50", reference_genome = "GRCh37") {
+  chromosomes = locusviz:::CHROMOSOME_SIZES[[reference_genome]]$chromosome
+  chromosome_colors = rep(c(odd_color, even_color), length.out = length(chromosomes)) %>%
     magrittr::set_names(chromosomes)
   return(scale_color_manual(values = chromosome_colors))
 }
